e4b6b5a36b6406bf03b6f66ee5191c341cda10db
Alain Boudard <coco.boudard@gmail.com>
2024-07-04 23:15:29 +0100

    fix: add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,33 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [starlight({
+    title: "Astro Qwik",
+    logo: {
+      src: "./src/assets/logo.svg"
+    },
+    social: {
+      github: "https://github.com/aboudard/astro-qwik"
+    },
+    sidebar: [{
+      label: "Guides",
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: "Example Guide",
+        link: "/guides/example/"
+      }]
+    }, {
+      label: "Reference",
+      autogenerate: {
+        directory: "reference"
+      }
+    }]
+  })],
+  output: "server",
+  adapter: netlify()
 });

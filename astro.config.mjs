9400361aa14a1c06aeff5c34c6408fa4f36e8865
aboudard <coco.boudard@gmail.com>
2025-06-18 10:35:35 +0200

    fix: first pages & config + style

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,29 +5,35 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: "Astro Qwik",
-    logo: {
-      src: "./src/assets/logo.svg"
-    },
-    social: {
-      github: "https://github.com/aboudard/astro-qwik"
-    },
-    sidebar: [{
-      label: "Guides",
-      items: [
-      // Each item here is one entry in the navigation menu.
-      {
-        label: "Example Guide",
-        link: "/guides/example/"
-      }]
-    }, {
-      label: "Reference",
-      autogenerate: {
-        directory: "reference"
-      }
-    }]
-  })],
+  integrations: [
+    starlight({
+      title: "Qwik Essentials",
+      customCss: [
+        // Relative path to your custom CSS file
+        "./src/styles/custom.css",
+      ],
+      logo: {
+        src: "./src/assets/logo.svg",
+      },
+      social: {
+        github: "https://github.com/aboudard/astro-qwik",
+      },
+      sidebar: [
+        {
+          label: "Getting started",
+          autogenerate: {
+            directory: "getting-started",
+          },
+        },
+        {
+          label: "Reference",
+          autogenerate: {
+            directory: "reference",
+          },
+        },
+      ],
+    }),
+  ],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
